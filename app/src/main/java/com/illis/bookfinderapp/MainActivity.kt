30ed9a48d273a5754924d25cae0fe4f1aabfda68
mr_kim <rofalfl42@gmail.com>
2022-12-28 23:15:35 +0900

    화면 전환을 간단히 하기 위해 navigation ui 라이브러리 추가

--- app/src/main/java/com/illis/bookfinderapp/MainActivity.kt
+++ app/src/main/java/com/illis/bookfinderapp/MainActivity.kt
@@ -1,12 +1,26 @@
 
 package com.illis.bookfinderapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.illis.bookfinderapp.data.repository.BookRepositoryImpl
+import com.illis.bookfinderapp.databinding.ActivityMainBinding
+import com.illis.bookfinderapp.domain.usecase.SearchUseCase
+import com.illis.bookfinderapp.presentation.SearchViewModel
+import com.illis.bookfinderapp.presentation.SearchViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var searchViewModel: SearchViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModelFactory = SearchViewModelFactory(SearchUseCase(BookRepositoryImpl()))
+        searchViewModel = ViewModelProvider(this, viewModelFactory)[SearchViewModel::class.java]
     }
 }
