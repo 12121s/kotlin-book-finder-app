15135b3f0e043cabb704fdeba581273aa508bb25
mr_kim <rofalfl42@gmail.com>
2022-12-29 18:18:16 +0900

    1. 한 페이지 결과 갯수 변경 2. 비즈니스 로직 인터페이스 추가(viewmodel에서 치리할 때 job을 받아서 처리할 필요가 없어서 return 없도록 수정)

--- app/src/main/java/com/illis/bookfinderapp/consts/ServerConsts.kt
+++ app/src/main/java/com/illis/bookfinderapp/consts/ServerConsts.kt
@@ -4,6 +4,6 @@ class ServerConsts {
     companion object {
         const val BASE_URL = "https://www.googleapis.com/"
 
-        const val BOOKS_API_MAX_RESULTS = 10;
+        const val BOOKS_API_MAX_RESULTS = 20;
     }
 }
--- app/src/main/java/com/illis/bookfinderapp/presentation/SearchViewModel.kt
+++ app/src/main/java/com/illis/bookfinderapp/presentation/SearchViewModel.kt
@@ -8,10 +8,17 @@ import com.illis.bookfinderapp.data.Resource
 import com.illis.bookfinderapp.data.model.Books
 import com.illis.bookfinderapp.data.model.VolumeInfo
 import com.illis.bookfinderapp.domain.usecase.SearchUseCase
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class SearchViewModel(private val searchUseCase: SearchUseCase) : ViewModel() {
+interface SearchStep {
+    fun getBooks(searchText: String)
+    fun getNextPageBooks()
+}
+
+class SearchViewModel(private val searchUseCase: SearchUseCase) : ViewModel(), SearchStep {
     var firstOpen = true
     val booksResponse = MutableLiveData<List<VolumeInfo>?>()
     val searchState = MutableLiveData<Resource<String>>()
@@ -22,46 +29,50 @@ class SearchViewModel(private val searchUseCase: SearchUseCase) : ViewModel() {
     private var page = 0 // 현재 페이지
     private var maxPage = 0 // 마지막 페이지
 
-    fun getBooks(searchText: String) = viewModelScope.launch(Dispatchers.IO) {
-        // 검색어, 페이지 초기화
-        this@SearchViewModel.searchText = searchText
-        page = 0
-        searchState.postValue(Resource.Loading())
-        when(val response: Resource<Books> = searchUseCase(searchText, page)) {
-            is Resource.Success -> {
-                val bookList = response.data?.bookInfoLists?.map { it.volumeInfo }
-                if (!bookList.isNullOrEmpty()) {
-                    searchState.postValue(Resource.Success("success"))
-                    booksResponse.postValue(bookList)
-                    volumeCount.postValue(response.data.totalItems)
+    override fun getBooks(searchText: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            // 검색어, 페이지 초기화
+            this@SearchViewModel.searchText = searchText
+            page = 0
+            searchState.postValue(Resource.Loading())
+            when(val response: Resource<Books> = searchUseCase(searchText, page)) {
+                is Resource.Success -> {
+                    val bookList = response.data?.bookInfoLists?.map { it.volumeInfo }
+                    if (!bookList.isNullOrEmpty()) {
+                        searchState.postValue(Resource.Success("success"))
+                        booksResponse.postValue(bookList)
+                        volumeCount.postValue(response.data.totalItems)
 
-                    maxPage = response.data.totalItems.div(ServerConsts.BOOKS_API_MAX_RESULTS)
-                } else {
-                    searchState.postValue(Resource.Error("No Result"))
+                        maxPage = response.data.totalItems.div(ServerConsts.BOOKS_API_MAX_RESULTS)
+                    } else {
+                        searchState.postValue(Resource.Error("No Result"))
 
-                    maxPage = 0
+                        maxPage = 0
+                    }
                 }
-            }
-            is Resource.Error -> {
-                searchState.postValue(Resource.Error(response.message?: "unknown error"))
-            }
-            else -> {
-                searchState.postValue(Resource.Success(""))
+                is Resource.Error -> {
+                    searchState.postValue(Resource.Error(response.message?: "unknown error"))
+                }
+                else -> {
+                    searchState.postValue(Resource.Success(""))
+                }
             }
         }
     }
 
-    fun getNextPageBooks() = viewModelScope.launch(Dispatchers.IO) {
-        if (page <= maxPage) {
-            val response: Resource<Books> = searchUseCase(searchText, ++page)
+    override fun getNextPageBooks() {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (page <= maxPage) {
+                val response: Resource<Books> = searchUseCase(searchText, ++page)
 
-            val tempBookList = booksResponse.value?.toMutableList()
-            val addBookList = response.data?.bookInfoLists?.map { it.volumeInfo }
-            if (!addBookList.isNullOrEmpty()) {
-                tempBookList?.addAll(addBookList)
-                booksResponse.postValue(tempBookList)
-            } else { // 더이상 추가할 값이 없으면 서버 요청 안하도록
-                maxPage = page - 1
+                val tempBookList = booksResponse.value?.toMutableList()
+                val addBookList = response.data?.bookInfoLists?.map { it.volumeInfo }
+                if (!addBookList.isNullOrEmpty()) {
+                    tempBookList?.addAll(addBookList)
+                    booksResponse.postValue(tempBookList)
+                } else { // 더이상 추가할 값이 없으면 서버 요청 안하도록
+                    maxPage = page - 1
+                }
             }
         }
     }
